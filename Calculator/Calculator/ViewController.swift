7ba53e9b5643729d05873847d4faa14edd101ec3
Onsemiro <soorink@gmail.com>
2022-08-02 01:14:03 +0900

    베이직 3 - 계산기
    베이직 3 - 계산기

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by SooRin Kim on 2021/12/29.
+//
+
+import UIKit
+
+enum Operation {
+    case Add
+    case Subtract
+    case Divide
+    case Multiply
+    case unknown
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    var result = ""
+    var currentOpeation: Operation = .unknown
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapNumberButton(_ sender: UIButton) {
+        guard let numberValue = sender.titleLabel?.text else { return }
+        if self.displayNumber.count < 9 {
+            self.displayNumber += numberValue
+            self.numberOutputLabel.text = self.displayNumber
+        }
+    }
+    
+    @IBAction func tabClearButton(_ sender: UIButton) {
+        self.displayNumber = ""
+        self.firstOperand = ""
+        self.secondOperand = ""
+        self.result = ""
+        self.currentOpeation = .unknown
+        self.numberOutputLabel.text = "0"
+    }
+    
+    @IBAction func tabDotButton(_ sender: UIButton) {
+        if self.displayNumber.count < 8, !self.displayNumber.contains(".") {
+            self.displayNumber += self.displayNumber.isEmpty ? "0." : "."
+            self.numberOutputLabel.text = self.displayNumber
+        }
+    }
+    
+    @IBAction func tapDivideButton(_ sender: UIButton) {
+        self.operation(.Divide)
+    }
+    
+    @IBAction func tapMultiplyButton(_ sender: UIButton) {
+        self.operation(.Multiply)
+    }
+    
+    @IBAction func tapSubtracButton(_ sender: UIButton) {
+        self.operation(.Subtract)
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIButton) {
+        self.operation(.Add)
+    }
+    
+    @IBAction func tapEqualButotn(_ sender: UIButton) {
+        self.operation(self.currentOpeation)
+    }
+    
+    func operation(_ operation: Operation) {
+        if self.currentOpeation != .unknown {
+            if !self.displayNumber.isEmpty {
+                self.secondOperand = self.displayNumber
+                self.displayNumber = ""
+                
+                guard let firstOperand = Double(self.firstOperand) else { return }
+                guard let secondOperand = Double(self.secondOperand) else { return }
+                
+                switch self.currentOpeation {
+                case .Add:
+                    self.result = "\(firstOperand + secondOperand)"
+                case .Subtract:
+                    self.result = "\(firstOperand - secondOperand)"
+                case .Divide:
+                    self.result = "\(firstOperand / secondOperand)"
+                case .Multiply:
+                    self.result = "\(firstOperand * secondOperand)"
+                default:
+                    break
+                }
+                
+                if let result = Double(self.result), result.truncatingRemainder(dividingBy: 1) == 0 {
+                    self.result = "\(Int(result))"
+                }
+                
+                self.firstOperand = self.result
+                self.numberOutputLabel.text = self.result
+            }
+            self.currentOpeation = operation
+        } else {
+            self.firstOperand = self.displayNumber
+            self.currentOpeation = operation
+            self.displayNumber = ""
+        }
+    }
+    
+}
+
