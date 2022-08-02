620c170f26eee5452f3d79dfd064c5405199f490
Onsemiro <soorink@gmail.com>
2022-08-02 20:30:19 +0900

    Basic 5 - 일기

---
+++ Diary/Diary/Diary.swift
@@ -0,0 +1,16 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by SooRin Kim on 2022/01/09.
+//
+
+import Foundation
+
+struct Diary {
+    var uuidString: String
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
